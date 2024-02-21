2b0e5a95bb4e7aab0320ef7c8c939bc312571877
Marcus Yang <yutiany@andrew.cmu.edu>
2024-02-21 14:22:14 -0800

    added statues update

--- config/socketConfig.mjs
+++ config/socketConfig.mjs
@@ -1,14 +1,26 @@
 import { Server } from 'socket.io';
 // const chatMsgModel = require("../models/ChatMsgModel");
 import * as postService from '../services/postService.mjs';
+import * as userService from '../services/userService.mjs';
 
 export default function (server) {
     const io = new Server(server);
+    let socketToUser = new Map();
 
     io.on("connection", (socket) => {
-        console.log("a user connected");
-        socket.on("disconnect", () => {
-            console.log("user disconnected");
+        console.log(`User ${socket.id} connected`);
+        socket.on("registered", (userId) => {
+            if (! socketToUser.has(socket.id)) {
+                socketToUser.set(socket.id, userId);
+            }
+            io.emit("status_update");
+        })
+        
+        socket.on("disconnect", async () => {
+            console.log(`user ${socketToUser.get(socket.id)} disconnected`);
+            await userService.changeOnlineStatus(socketToUser.get(socket.id), "Offline").then((res)=>{
+                console.log(res);
+            })
         });
         socket.on("postData", (postData) => {
             io.emit("postData", postData);
