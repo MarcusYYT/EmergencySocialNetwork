b0a5d13056357ca9aa4adda8c5825c32bd2f47b6
sonyemehS2024 <sonyemeh@andrew.cmu.edu>
2024-02-20 17:45:44 -0800

    get posts functionality working
    Co-authored-by: Kai Huang <kaihuan2S2024@users.noreply.github.com>
    Co-authored-by: Marcus Yang <yutianyS2024@users.noreply.github.com>

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,33 +1,46 @@
-class ChatMessage {
-    constructor(sender, message, dateTime) {
-        this.sender = sender;
-        this.message = message;
-        this.time = dateTime;
-    }
-    
-    createMessageElement() {
+export function constructChatMessage(sender, message, status, dateTime) {
+
         const messageDiv = document.createElement('div');
         messageDiv.className = 'list-group-item';
         const messageHeader = document.createElement('div');
         messageHeader.className = 'd-flex w-100 justify-content-between';
         const senderSpan = document.createElement('span');
         senderSpan.className = 'message-sender';
-        senderSpan.textContent = this.sender;
+        senderSpan.textContent = sender;
         const dateSpan = document.createElement('span');
         dateSpan.className = 'message-date';
-        dateSpan.textContent = this.time;
+        dateSpan.textContent = new Date(dateTime).toLocaleString;
+
+        const statusSpan = document.createElement('span');
+        statusSpan.className = 'message-status';
+        statusSpan.textContent = status;
+
         messageHeader.appendChild(senderSpan);
         messageHeader.appendChild(dateSpan);
+        messageHeader.appendChild(statusSpan);
+
+
         const messageBody = document.createElement('div');
         messageBody.className = 'message-body';
-        messageBody.textContent = this.message;
+        messageBody.textContent = message;
         messageDiv.appendChild(messageHeader);
         messageDiv.appendChild(messageBody);
         return messageDiv;
-    }
+
 }
-// Exporting the ChatMessage class
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = ChatMessage;
-}
+// // Exporting the ChatMessage class
+// if (typeof module !== 'undefined' && module.exports) {
+//     module.exports = ChatMessage;
+// }
+
+const datetimeFormatter = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZoneName: 'short'
+  });
+d
 
---
+++ public/javascripts/posts.js
@@ -0,0 +1,62 @@
+function constructChatMessage(sender, message, status, dateTime) {
+
+    const messageDiv = document.createElement('div');
+    messageDiv.className = 'list-group-item';
+    const messageHeader = document.createElement('div');
+    messageHeader.className = 'd-flex w-100 justify-content-between';
+    const messageUsernameHeader = document.createElement('div');
+    messageUsernameHeader.className = 'd-flex w-100 justify-content-between';
+    const senderSpan = document.createElement('span');
+    senderSpan.className = 'message-sender';
+    senderSpan.textContent = sender;
+    const dateSpan = document.createElement('span');
+    dateSpan.className = 'message-date';
+    dateSpan.textContent = new Date(dateTime).toLocaleString();
+
+    const statusSpan = document.createElement('span');
+    statusSpan.className = 'message-status';
+    statusSpan.textContent = status;
+
+    messageUsernameHeader.appendChild(senderSpan);
+    messageHeader.appendChild(dateSpan);
+    messageUsernameHeader.appendChild(statusSpan);
+
+    const messageBody = document.createElement('div');
+    messageBody.className = 'message-body';
+    messageBody.textContent = message;
+    messageDiv.appendChild(messageUsernameHeader);
+    messageDiv.appendChild(messageHeader);
+    messageDiv.appendChild(messageBody);
+    return messageDiv;
+
+}
+
+async function renderChats(chatlist) {
+
+    let messageBoard = document.getElementById("message-board")
+
+    for (const msgData of chatlist) {
+
+        
+        let messageElement = constructChatMessage(
+            msgData.user_id,
+            msgData.content,
+            msgData.status,
+            msgData.createdAt
+        );
+
+        messageBoard.appendChild(messageElement);
+    }
+}
+
+window.addEventListener("load", async () => {
+    await fetch('/posts')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            renderChats(data.data)
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}, false)
