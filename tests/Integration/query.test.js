a8b279ffe2cc9d857cd343c3c779d9ad5b5506c4
Marcus Yang <yutiany@andrew.cmu.edu>
2024-03-12 19:40:58 -0700

    temp
    Co-Authored-By: Haoming Yuan <157338874+hyuan4S2024@users.noreply.github.com>

--- tests/Integration/query.test.js
+++ tests/Integration/query.test.js
@@ -1,66 +1,100 @@
 import { validUser, createNewUser, changeStatus, changeOnlineStatus } from "../../services/userService.mjs";
 import {User} from "../../models/User.model.mjs";
+import {Post} from "../../models/Post.model.mjs";
+import {PrivatePost} from "../../models/PrivatePost.model.mjs";
+import {Status} from "../../models/Status.model.mjs";
 import {response} from "express";
-beforeAll(async () => {
-    await User.sync();
+import fetch from 'node-fetch';
+import { createServer } from "http";
+import app from "../../app.mjs";
+
+const server = createServer(app);
+const serverUrl = `http:// localhost:3000`;
+beforeAll(() => {
+    server.listen(4000, async () => {
+        done();
+    })
 });
 
-describe('User registration and login', () => {
+afterAll((done) =>{
+    server.close(done);
+})
 
-    test('User registration', async () => {
-        expect(await createNewUser('Carnegie', 'Mellon')).toMatchObject({
-            success: true,
-            user_id: expect.any(Number),
-            message: "Create user successfully."
+describe("Register user", () => {
+    test('user registration', async() => {
+        const response = await fetch(`${serverUrl}/auth/register`, {
+            method: 'POST',
+            header: {
+                'Content-Type' : 'application/json',
+            },
+            body: JSON.stringify({username: "testuser", password: "test"})
         });
-    });
-
-    test('User login with correct credential', async () => {
-        expect(await validUser('Carnegie', 'Mellon')).toMatchObject({
-            code: 200,
-            user_id: expect.any(Number),
-        });
-    });
-
-    test('User login with incorrect credential', async () => {
-        expect(await validUser('Carnegie', 'Mellon2')).toMatchObject({
-            code: 401,
-            user_id: null,
-        });
-    });
-
-    test('User login with unexist user', async () => {
-        expect(await validUser('Carnegie22', 'Mellon')).toMatchObject({
-            code: 404,
-            user_id: null,
-        });
-    });
+        const data = await response.json();
+        console.log(data);
+        expect(data).toMatchObject(expect.any(Object));
+        
+    })
 });
 
-describe('User status update', () => {
-    let sharedUserId;
-    test('User registration', async () => {
-        const response = await createNewUser('Carnegieeee', 'Mellon');
-        expect(response).toMatchObject({
-            success: true,
-            user_id: expect.any(Number),
-            message: "Create user successfully."
-        });
-        sharedUserId = response.user_id;
-    });
 
-    test('User status update to emergency', async () => {
-        expect(await changeStatus(sharedUserId, 'emergency')).toMatchObject({
-            success: true,
-            message: "Change status successfull"
-        });
-    });
 
-    test('User online status update to offline', async () => {
-        expect(await changeOnlineStatus(sharedUserId, 'offline')).toMatchObject({
-            success: true,
-            message: `Change online for user ${sharedUserId} status successfull`
-        });
-    });
 
-});
+// describe('User registration and login', () => {
+
+//     test('User registration', async () => {
+//         expect(await createNewUser('Carnegie', 'Mellon')).toMatchObject({
+//             success: true,
+//             user_id: expect.any(Number),
+//             message: "Create user successfully."
+//         });
+//     });
+
+//     test('User login with correct credential', async () => {
+//         expect(await validUser('Carnegie', 'Mellon')).toMatchObject({
+//             code: 200,
+//             user_id: expect.any(Number),
+//         });
+//     });
+
+//     test('User login with incorrect credential', async () => {
+//         expect(await validUser('Carnegie', 'Mellon2')).toMatchObject({
+//             code: 401,
+//             user_id: null,
+//         });
+//     });
+
+//     test('User login with unexist user', async () => {
+//         expect(await validUser('Carnegie22', 'Mellon')).toMatchObject({
+//             code: 404,
+//             user_id: null,
+//         });
+//     });
+// });
+
+// describe('User status update', () => {
+//     let sharedUserId;
+//     test('User registration', async () => {
+//         const response = await createNewUser('Carnegieeee', 'Mellon');
+//         expect(response).toMatchObject({
+//             success: true,
+//             user_id: expect.any(Number),
+//             message: "Create user successfully."
+//         });
+//         sharedUserId = response.user_id;
+//     });
+
+//     test('User status update to emergency', async () => {
+//         expect(await changeStatus(sharedUserId, 'emergency')).toMatchObject({
+//             success: true,
+//             message: "Change status successfull"
+//         });
+//     });
+
+//     test('User online status update to offline', async () => {
+//         expect(await changeOnlineStatus(sharedUserId, 'offline')).toMatchObject({
+//             success: true,
+//             message: `Change online for user ${sharedUserId} status successfull`
+//         });
+//     });
+
+// });
