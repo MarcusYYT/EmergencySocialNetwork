5d9ee543774dbbe003a2eb03d05e2b692307c091
sonyemehS2024 <sonyemeh@andrew.cmu.edu>
2024-03-22 17:05:45 -0700

    added icons to the status dropdown

--- public/javascripts/directory.js
+++ public/javascripts/directory.js
@@ -15,62 +15,13 @@ function renderMyStatus(username, status) {
     let userInfoText = document.createTextNode("My current status is: " + status);
     userInfo.appendChild(userInfoText);
     
-    let statusForm = document.getElementById("status-form");
+    let statusForm = document.getElementById("status-list");
     profile.insertBefore(user, statusForm);
     profile.insertBefore(userInfo, statusForm);
 }
-
-// function renderESNList(list) {
-//     let directory = document.getElementById("directory");
-//     while(directory.firstChild){
-//       directory.removeChild(directory.lastChild);
-//     }
-    
-//     for (let i = 0; i < list.length; i++) {
-//         let directory = document.getElementById("directory");
-//         let userdiv = document.createElement("div");
-//         userdiv.setAttribute("class", "directory-user");
-//         userdiv.addEventListener("click", routeToPrivateChat(list[i].username))
-
-//         let usernameField = document.createElement("p");
-//         usernameField.setAttribute("class", "usernameField");
-//         let usernameFieldText = document.createTextNode(list[i].username);
-//         usernameField.appendChild(usernameFieldText);
-
-//         let onlineStatusField = document.createElement("p");
-//         onlineStatusField.setAttribute("class", "onlineStatusField");
-//         let onlineStatusFieldText = document.createTextNode(list[i].online_status);
-//         onlineStatusField.appendChild(onlineStatusFieldText);
-
-//         if (list[i].online_status === "online") {
-//             onlineStatusField.classList.add("online");
-//         }
-//         else {
-//             onlineStatusField.classList.add("offline");
-//         }
-
-//         let statusField = document.createElement("p");
-//         statusField.setAttribute("class", "statusField");
-//         let statusFieldText = document.createTextNode(list[i].status);
-//         statusField.appendChild(statusFieldText);
-
-//         userdiv.appendChild(usernameField);
-//         userdiv.appendChild(onlineStatusField);
-//         userdiv.appendChild(statusField);
-//         directory.appendChild(userdiv);
-//     }
-// }
 
 function routeToPrivateChat(sender, receiver){
     //await changeReadStatus(sender, receiver)
     window.location.href = `/privatePostsWall/${sender}/${receiver}`;
 }
 
-// async function changeReadStatus(sender, receiver) {
-//     await fetch(`/privatePosts/${sender}/${receiver}`, {
-//         method: 'PUT',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         }
-//     })
-// }
