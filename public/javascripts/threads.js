ac7081b317f7ec3fc71185b92f5331c873f18190
sonyemehS2024 <sonyemeh@andrew.cmu.edu>
2024-04-05 23:19:05 -0700

    thread styling

--- public/javascripts/threads.js
+++ public/javascripts/threads.js
@@ -1,134 +1,123 @@
-function constructChatMessage(msgData) {
-    const messageDiv = document.createElement('div');
-    messageDiv.className = 'list-group-item';
-    const messageHeader = document.createElement('div');
-    messageHeader.className = 'd-flex w-100 justify-content-between';
-    const messageUsernameHeader = document.createElement('div');
-    messageUsernameHeader.className = 'd-flex w-100 justify-content-between';
-    const senderSpan = document.createElement('span');
-    senderSpan.className = 'message-sender';
-    senderSpan.textContent = msgData.sender;
-    const dateSpan = document.createElement('span');
-    dateSpan.className = 'message-date';
-    dateSpan.textContent = new Date(msgData.dateTime).toLocaleString();
-    const statusFieldImage = document.createElement("i");
-    if (msgData.status == "OK") {
-      statusFieldImage.setAttribute("class", "bi bi-check-circle-fill");
-    } else if (msgData.status == "emergency") {
-      statusFieldImage.setAttribute("class", "bi bi-bandaid-fill");
-    } else if (msgData.status == "help") {
-      statusFieldImage.setAttribute("class", "bi bi-exclamation-circle-fill");
+function constructThread(msgData) {
+    const threadDiv = document.createElement('div');
+    threadDiv.className = 'thread row gx-1 justify-content-between';
+    const threadHeader = document.createElement('div');
+    threadHeader.className = 'thread-name';
+    threadHeader.appendChild(document.createTextNode(msgData.thread_name))
+    const threadCreatorHeader = document.createElement('div');
+    threadCreatorHeader.className = 'creator col-11';
+    const creatorSpan = document.createElement('span');
+    creatorSpan.className = 'thread-creator';
+    creatorSpan.appendChild(document.createTextNode("By: " + msgData.creator));
+    const urgencyFieldImage = document.createElement("i");
+    if (msgData.urgency === "Low Priority") {
+      urgencyFieldImage.setAttribute("class", "col-1 bi bi-check-circle-fill");
+    } else if (msgData.urgency === "High Priority") {
+      urgencyFieldImage.setAttribute("class", "col-1 bi bi-bandaid-fill");
+    } else if (msgData.urgency === "Normal Priority") {
+      urgencyFieldImage.setAttribute("class", "col-1 bi bi-exclamation-circle-fill");
     }
-    messageUsernameHeader.appendChild(senderSpan);
-    messageHeader.appendChild(dateSpan);
-    messageUsernameHeader.appendChild(statusFieldImage);
-    messageDiv.appendChild(messageUsernameHeader);
-    messageDiv.appendChild(messageHeader);
-    if (!msgData.isStatus){
-      const messageBody = document.createElement('div');
-      messageBody.className = 'message-body';
-      messageBody.textContent = msgData.message;
-      messageDiv.appendChild(messageBody);  
-    }
-    return messageDiv;
+
+    threadCreatorHeader.appendChild(threadHeader);
+    threadCreatorHeader.appendChild(creatorSpan);
+    threadDiv.appendChild(threadCreatorHeader);
+    threadDiv.appendChild(urgencyFieldImage);
+    return threadDiv;
   }
   
-  async function renderThreads(chatlist) {
-    let messageBoard = document.getElementById("message-board")
-    let isStatus = false;
-    removePostElements(messageBoard)
-    for (const msgData of chatlist) {
+  async function renderThreads(threadlist) {
+    let threadWrapper = document.getElementById("threadWrapper")
+    removeChildElements(threadWrapper)
+    for (const msgData of threadlist) {
       let username = ""
      
-      username = msgData.user.username;
+      username = msgData.Creator.username;
       
-      let messageDetails = createMsgObject(msgData, username, isStatus);
-      let messageElement = constructChatMessage(messageDetails);
-      messageBoard.appendChild(messageElement);
-      messageBoard.scrollTop = messageBoard.scrollHeight;
+      let messageDetails = createThreadObject(msgData, username);
+      let messageElement = constructThread(messageDetails);
+      threadWrapper.appendChild(messageElement);
+      threadWrapper.scrollTop = threadWrapper.scrollHeight;
     }
   }
   
-  function slice(array, size){   
-    let slicedArray = [];
-    for (let i = 0; i < Math.ceil(array.length / size); i++) {
-        slicedArray.push(array.slice(i * size, i * size + size));
-    }
-    return slicedArray 
-  }
+//   function slice(array, size){   
+//     let slicedArray = [];
+//     for (let i = 0; i < Math.ceil(array.length / size); i++) {
+//         slicedArray.push(array.slice(i * size, i * size + size));
+//     }
+//     return slicedArray 
+//   }
   
   let counter = 0;
   
-  function renderSlicedArray(slicedArray){
-    let messageBoard = document.getElementById("message-board")
-    let showMore = document.getElementById("show-more");
-    if (showMore) {
-        showMore.remove();
-    }
-    for (let i = 0; i < slicedArray[counter].length; i++) {
-      let msgData = slicedArray[counter][i];
+//   function renderSlicedArray(slicedArray){
+//     let messageBoard = document.getElementById("message-board")
+//     let showMore = document.getElementById("show-more");
+//     if (showMore) {
+//         showMore.remove();
+//     }
+//     for (let i = 0; i < slicedArray[counter].length; i++) {
+//       let msgData = slicedArray[counter][i];
   
-      let username = "";
+//       let username = "";
 
-      username = msgData.creator.username;
+//       username = msgData.creator.username;
     
-      let messageDetails = createMsgObject(msgData, username, isStatus)
-      let messageElement = constructChatMessage(messageDetails);
-      messageBoard.appendChild(messageElement);
-    }
-    if (counter + 1 < slicedArray.length && !isStatus) {
-        createShowMore(slicedArray, isPrivate)
-        counter++;  
-    }
-  }
+//       let messageDetails = createMsgObject(msgData, username, isStatus)
+//       let messageElement = constructChatMessage(messageDetails);
+//       messageBoard.appendChild(messageElement);
+//     }
+//     if (counter + 1 < slicedArray.length && !isStatus) {
+//         createShowMore(slicedArray, isPrivate)
+//         counter++;  
+//     }
+//   }
   
-  function createShowMore(slicedArray, isPrivate){
-    let messageBoard = document.getElementById("message-board")
-    let showMore = document.createElement("div");
-    showMore.setAttribute("id", "show-more")
-    showMore.setAttribute("class", "list-group-item")
-    let showMoreText = document.createTextNode("Show More...")  
-    showMore.addEventListener("click", () => {renderSlicedArray(slicedArray, isPrivate)})
-    showMore.appendChild(showMoreText)
-    messageBoard.appendChild(showMore)
-  }
+//   function createShowMore(slicedArray, isPrivate){
+//     let messageBoard = document.getElementById("message-board")
+//     let showMore = document.createElement("div");
+//     showMore.setAttribute("id", "show-more")
+//     showMore.setAttribute("class", "list-group-item")
+//     let showMoreText = document.createTextNode("Show More...")  
+//     showMore.addEventListener("click", () => {renderSlicedArray(slicedArray, isPrivate)})
+//     showMore.appendChild(showMoreText)
+//     messageBoard.appendChild(showMore)
+//   }
   
-  async function renderSearchedPosts(chatlist, isPrivate, isStatus) {
-    counter = 0;
-    let messageBoard = document.getElementById("message-board")
-    removePostElements(messageBoard);
-    if(chatlist.length == 0){
-      renderEmptyMessage()
-    } else {
-      const sliceSize = 10;
-      let slicedArray = slice(chatlist, sliceSize)
-      renderSlicedArray(slicedArray, isPrivate, isStatus);    
-    }  
-  }
+//   async function renderSearchedPosts(chatlist, isPrivate, isStatus) {
+//     counter = 0;
+//     let messageBoard = document.getElementById("message-board")
+//     removeChildElements(messageBoard);
+//     if(chatlist.length == 0){
+//       renderEmptyMessage()
+//     } else {
+//       const sliceSize = 10;
+//       let slicedArray = slice(chatlist, sliceSize)
+//       renderSlicedArray(slicedArray, isPrivate, isStatus);    
+//     }  
+//   }
   
   function renderEmptyMessage(){
-    let messageBoard = document.getElementById("message-board")
-    removePostElements(messageBoard)
+    let threadWrapper = document.getElementById("threadWrapper")
+    removeChildElements(threadWrapper)
     let emptyMessage = document.createElement("h1")
     let emptyMessageText = document.createTextNode("No results found")
     emptyMessage.className = 'empty-message'
     emptyMessage.appendChild(emptyMessageText)
-    messageBoard.appendChild(emptyMessage);
+    threadWrapper.appendChild(emptyMessage);
   }
   
-  function createMsgObject(msgData, username, isStatus) {
+  function createThreadObject(msgData, username) {
     let messageDetails = {
-      sender: username,
-      message: msgData.content,
-      status: msgData.status,
-      dateTime: msgData.createdAt,
-      isStatus: isStatus
+      creator: username,
+      thread_name: msgData.thread_name,
+      urgency: msgData.urgency,
     }
     return messageDetails;
   }
   
-  function removePostElements(messageBoard) {
-    while (messageBoard.firstChild) {
-      messageBoard.removeChild(messageBoard.lastChild);
+  function removeChildElements(element) {
+    while (element.firstChild) {
+        element.removeChild(element.lastChild);
     }
   }
