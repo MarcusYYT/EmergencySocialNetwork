2fb543dd60de7e1fe5ee856ae7a6e7256759f938
Kai Huang <kaihuan2@andrew.cmu.edu>
2024-04-07 14:58:07 -0700

    Add alert error handling when user does not follow the emergency contact rules

--- public/javascripts/emergencyContact.js
+++ public/javascripts/emergencyContact.js
@@ -1,35 +1,36 @@
 function renderEmergencyContact(primary, alternative, message) {
-    let profile = document.getElementById("emergency-wrapper");
-
+    let primayContactForm = document.getElementById("primary-contact-form");
     if (document.getElementById("primary-contact") != undefined) {
-        profile.removeChild(document.getElementById("primary-contact"))
+        primayContactForm.removeChild(document.getElementById("primary-contact"))
     }
     let primaryInfo = document.createElement("p");
     primaryInfo.setAttribute("id", "primary-contact");
-    let primaryInfoText = document.createTextNode("My Primary Contact is: " + primary);
+    let primaryInfoText = document.createTextNode(primary);
     primaryInfo.appendChild(primaryInfoText);
 
+    let alternativeContactForm = document.getElementById("alternative-contact-form");
     if (document.getElementById("alternative-contact") != undefined) {
-        profile.removeChild(document.getElementById("alternative-contact"))
+        alternativeContactForm.removeChild(document.getElementById("alternative-contact"))
     }
     let alternativeInfo = document.createElement("p");
     alternativeInfo.setAttribute("id", "alternative-contact");
-    let alternativeInfoText = document.createTextNode("My Alternative Contact is: " + alternative);
+    let alternativeInfoText = document.createTextNode(alternative);
     alternativeInfo.appendChild(alternativeInfoText);
 
+    let emergencyMessageForm = document.getElementById("emergency-message-form");
     if (document.getElementById("user-message") != undefined) {
-        profile.removeChild(document.getElementById("user-message"))
+        emergencyMessageForm.removeChild(document.getElementById("user-message"))
     }
     let emergencyMessage = document.createElement("p");
     emergencyMessage.setAttribute("id", "user-message");
-    let emergencyMessageText = document.createTextNode("My Emergency Message is: " + message);
+    let emergencyMessageText = document.createTextNode(message);
     emergencyMessage.appendChild(emergencyMessageText);
     
-    let primayContactForm = document.getElementById("primary-contact-form");
-    profile.insertBefore(primaryInfo, primayContactForm);
-    let alternativeContactForm = document.getElementById("alternative-contact-form");
-    profile.insertBefore(alternativeInfo, alternativeContactForm);
-    let emergencyMessageForm = document.getElementById("emergency-message-form");
-    profile.insertBefore(emergencyMessage, emergencyMessageForm);
+    let primayContactInput = document.getElementById("primary-input");
+    primayContactForm.insertBefore(primaryInfo, primayContactInput);
+    let alternativeContactInput = document.getElementById("alternative-input");
+    alternativeContactForm.insertBefore(alternativeInfo, alternativeContactInput);
+    let emergencyMessageInput = document.getElementById("emergency-message");
+    emergencyMessageForm.insertBefore(emergencyMessage, emergencyMessageInput);
     
 }
