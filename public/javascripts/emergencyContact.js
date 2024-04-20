5f1ad25c72b46cc83787e8ffcf38cf65812e72e6
Kai Huang <kaihuan2@andrew.cmu.edu>
2024-04-20 13:48:09 -0700

    Update emergency contact.js to reduce unit complexity

--- public/javascripts/emergencyContact.js
+++ public/javascripts/emergencyContact.js
@@ -57,15 +57,6 @@ function getLocation() {
                 case error.PERMISSION_DENIED:
                     console.error("User denied the request for geolocation.");
                     break;
-                case error.POSITION_UNAVAILABLE:
-                    console.error("Location information is unavailable.");
-                    break;
-                case error.TIMEOUT:
-                    console.error("The request to get user location timed out.");
-                    break;
-                case error.UNKNOWN_ERROR:
-                    console.error("An unknown error occurred.");
-                    break;
             }
         });
     } else {
